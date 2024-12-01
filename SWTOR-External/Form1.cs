ad472006fb13ddcfc3533445ab31f65866a05379
NFC <109616268+NightfallChease@users.noreply.github.com>
2024-12-01 21:39:50 +0100

    Small fix
    idk what exactly

--- SWTOR-External/Form1.cs
+++ SWTOR-External/Form1.cs
@@ -37,7 +37,7 @@ namespace SWTOR_External
         private Vector3 lastPos;
         private bool darkmodeEnabled = false;
         private string urlRunning = "https://github.com/NightfallChease/s/blob/main/isRunning.sw";
-        private string urlUpdate = "https://github.com/NightfallChease/s/blob/main/version8.6.sw";
+        private string urlUpdate = "https://github.com/NightfallChease/s/blob/main/version8.7.sw";
         private string currentVersion = "v8.7";
         private bool noclipPatched = false;
         private bool cameraPatched = false;
